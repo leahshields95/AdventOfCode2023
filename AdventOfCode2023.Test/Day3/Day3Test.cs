34584fd8151b0addeff96377bf4da344b0af176d
leahs <leah.shields@spektrix.com>
2023-12-03 20:20:01 +0000

    Day 2 very messy solution

--- AdventOfCode2023.Test/Day3/Day3Test.cs
+++ AdventOfCode2023.Test/Day3/Day3Test.cs
@@ -16,27 +16,183 @@ public class Tests
     }
 
     #region Part1
+
     [Test]
-    public void ShouldDoSomethingWithPuzzleInput()
+    public void ShouldReturnTrueForSymbols()
+    {
+        Assert.True(_day3.IsSymbol('$'));
+        Assert.True(_day3.IsSymbol('*'));
+        Assert.True(_day3.IsSymbol('&'));
+        Assert.True(_day3.IsSymbol('!'));
+        Assert.True(_day3.IsSymbol('#'));
+        Assert.True(_day3.IsSymbol('/'));
+        Assert.True(_day3.IsSymbol('='));
+        Assert.True(_day3.IsSymbol('@'));
+    }
+
+    [Test]
+    public void ShouldReturnFalseForFullStopAndDigits()
+    {
+        Assert.False(_day3.IsSymbol('.'));
+        Assert.False(_day3.IsSymbol('2'));
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfNumberAdjacentToSymbol()
+    {
+        var firstLine = new[] { '1', '*', '.' };
+        var secondLine = new[] { '2', '3', '4' };
+
+        var result = _day3.IsPartNumber(new[] { 0 }, firstLine, secondLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfNumberAdjacentToSymbolDirectlyBelow()
+    {
+        var firstLine = new[] { '1', '2', '3' };
+        var secondLine = new[] { '.', '*', '.' };
+
+        var result = _day3.IsPartNumber(new[] { 0, 1, 2 }, firstLine, secondLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfNumberAdjacentToSymbolDiagonallyBelow()
+    {
+        var firstLine = new[] { '1', '.', '.' };
+        var secondLine = new[] { '.', '$', '.' };
+
+        var result = _day3.IsPartNumber(new[] { 0 }, firstLine, secondLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfNumberAdjacentToSymbolDirectlyAbove()
+    {
+        var firstLine = new[] { '.', '!', '.' };
+        var secondLine = new[] { '.', '1', '.' };
+
+        var result = _day3.IsPartNumber(new[] { 1 }, secondLine, firstLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfNumberAdjacentToSymbolDiagonallyAbove()
+    {
+        var firstLine = new[] { '!', '.', '.' };
+        var secondLine = new[] { '.', '1', '.' };
+
+        var result = _day3.IsPartNumber(new[] { 1 }, secondLine, firstLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnTrueIfThreeDigitNumberAdjacentToSymbol()
+    {
+        var firstLine = new[] { '1', '2', '3' };
+        var secondLine = new[] { '.', '&', '.' };
+
+        var result = _day3.IsPartNumber(new[] { 0 }, firstLine, secondLine);
+        Assert.True(result);
+    }
+
+    [Test]
+    public void ShouldReturnFalseIfNumberNotAdjacentToSymbol()
+    {
+        var firstLine = new[] { '1', '.', '.' };
+        var secondLine = new[] { '2', '3', '4' };
+
+        var result = _day3.IsPartNumber(new[] { 0 }, firstLine, secondLine);
+        Assert.False(result);
+    }
+
+    [Test]
+    public void ShouldReturnFalseIfNumberNotAdjacentToSymbolMultipleLines()
+    {
+        var firstLine = new[] { '1', '.', '.' };
+        var secondLine = new[] { '2', '3', '4' };
+        var thirdLine = new[] { '2', '3', '4' };
+
+        var result = _day3.IsPartNumber(new[] { 0, 1, 2 }, secondLine, firstLine, thirdLine);
+
+        Assert.False(result);
+    }
+
+    [Test]
+    public void ShouldReturnThreeDigitNumber()
+    {
+        var line = new[] { '.', '.', '2', '3', '2', '.', '5' };
+
+        var result = _day3.GetNumberFromChars(2, line);
+        Assert.That(result, Is.EqualTo("232"));
+    }
+
+    [Test]
+    public void ShouldGetSumOfAllPartNumbers()
+    {
+        Assert.That(_day3.Part1(), Is.EqualTo(4361));
+    }
+
+    [Test]
+    public void ShouldSumAllPartNumbersInPuzzleInput()
     {
         var input = Constants.BasePath + "day3_actual.txt";
         _day3 = new Day3(input);
 
-        Assert.That(_day3.Part1(), Is.EqualTo(0));
+        Assert.That(_day3.Part1(), Is.EqualTo(509115));
     }
 
     #endregion
 
     #region Part2
-    
 
     [Test]
-    public void ShouldFindPowerOfCubesFromPuzzleInput()
+    public void ShouldGetNumberWhenStartUnknown()
+    {
+        var firstLine = new[] { '.', '.', '2', '3', '5', '.', '5' };
+        var secondLine = new[] { '4', '6', '7', '.', '.', '1' };
+
+        Assert.That(_day3.GetNumberFromChars(3, firstLine), Is.EqualTo("235"));
+        Assert.That(_day3.GetNumberFromChars(2, secondLine), Is.EqualTo("467"));
+    }
+
+    [Test]
+    public void ShouldReturnGearRatioSingleNumbers()
+    {
+        var firstLine = new[] { '4', '*', '7', '.' };
+
+        _day3._data = new List<char[]> { firstLine };
+
+        var gearRatios = _day3.GetGearRatio(1, 0);
+        Assert.That(gearRatios, Is.EqualTo(28));
+    }
+
+    [Test]
+    public void ShouldReturnGearRatio()
+    {
+        var firstLine = new[] { '4', '6', '7', '.', '.', '1' };
+        var secondLine = new[] { '.', '.', '.', '*', '.', '.' };
+        var thirdLine = new[] { '.', '.', '3', '5', '.', '.' };
+
+        _day3._data = new List<char[]>() { firstLine, secondLine, thirdLine };
+        var gearRatios = _day3.GetGearRatio(3, 1);
+        Assert.That(gearRatios, Is.EqualTo(16345));
+    }
+    
+    [Test]
+    public void ShouldSumAllGearRatios()
+    {
+        Assert.That(_day3.Part2(), Is.EqualTo(467835));
+    }
+
+    [Test]
+    public void ShouldSumAllGearRatiosFromPuzzleInput()
     {
         var input = Constants.BasePath + "day3_actual.txt";
         _day3 = new Day3(input);
 
-        Assert.That(_day3.Part2(), Is.EqualTo(0));
+        Assert.That(_day3.Part2(), Is.EqualTo(75220503));
     }
 
     #endregion
--- AdventOfCode2023/Day3/Day3.cs
+++ AdventOfCode2023/Day3/Day3.cs
@@ -4,26 +4,270 @@ namespace AdventOfCode2023.Day3
 {
     public class Day3 : IChallenge
     {
-        private List<string> _data = new();
+        public List<char[]> _data { get; set; } = new();
 
-        private void DoSomethingWithLine(String line)
+        private void AddLineToList(String line)
         {
-            throw new NotImplementedException();
+            _data.Add(line.ToCharArray());
         }
 
         public Day3(String input)
         {
-            FileHelper.ReadFromInputFile(input, (line) => DoSomethingWithLine(line));
+            FileHelper.ReadFromInputFile(input, AddLineToList);
         }
 
         public int Part1()
         {
-            throw new NotImplementedException();
+            List<string> partNumbers = GetAllPartNumbers();
+
+            return partNumbers.Sum(Int32.Parse);
+        }
+
+        public string GetNumberFromChars(int index, char[] line)
+        {
+            for (int i = index; i >= 0; i--)
+            {
+                if (!char.IsNumber(line[i]))
+                {
+                    return GetNumberFromCharsFromStart(i + 1, line);
+                }
+            }
+            return GetNumberFromCharsFromStart(0, line);
+        }
+
+        public string GetNumberFromCharsFromStart(int index, char[] line)
+        {
+            var number = line[index].ToString();
+            
+            for (int i = index + 1; i < line.Length; i++)
+            {
+                if (char.IsNumber(line[i]))
+                {
+                    number += line[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return number;
+        }
+
+        private List<string> GetAllPartNumbers()
+        {
+            List<string> partNumbers = new();
+            for (int i = 0; i < _data.Count; i++)
+            {
+                var line = _data[i];
+                var isFirstLine = i == 0;
+                var isLastLine = i == _data.Count - 1;
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var digit = line[j];
+                    if (char.IsNumber(digit))
+                    {
+                        string number = GetNumberFromChars(j, line);
+
+                        List<char[]> adjacentLines = new();
+                        if (!isFirstLine) adjacentLines.Add(_data[i - 1]);
+                        if (!isLastLine) adjacentLines.Add(_data[i + 1]);
+
+                        if (IsPartNumber(new[] { j, j + number.Length - 1 }, line, adjacentLines.ToArray()))
+                        {
+                            partNumbers.Add(number);
+                        }
+
+                        j += number.Length - 1;
+                    }
+                }
+            }
+
+            return partNumbers;
         }
 
         public int Part2()
         {
-            throw new NotImplementedException();
+            List<int> gearRatios = new();
+            for (int i = 0; i < _data.Count; i++)
+            {
+                var line = _data[i];
+                // line.Contains('*')
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var digit = line[j];
+                    if (digit.Equals('*'))
+                    {
+                        gearRatios.Add(GetGearRatio(j, i));
+                    }
+                }
+            }
+
+            return gearRatios.Sum();
+        }
+
+        public bool IsPartNumber(int[] indices, char[] line, params char[][] adjacentLines)
+        {
+            // If adjacent on same line
+            var firstIndex = indices[0];
+
+            var isStartOfLine = firstIndex == 0;
+            if (!isStartOfLine && IsSymbol(line[firstIndex - 1]))
+            {
+                return true;
+            }
+
+            var lastIndex = indices[^1];
+            var isEndOfLine = lastIndex == line.Length - 1;
+
+            if (!isEndOfLine && IsSymbol(line[lastIndex + 1]))
+            {
+                return true;
+            }
+
+            foreach (var adjacentLine in adjacentLines)
+            {
+                // Check diagonals
+                if (!isStartOfLine && IsSymbol(adjacentLine[firstIndex - 1]))
+                {
+                    return true;
+                }
+
+                if (!isEndOfLine && IsSymbol(adjacentLine[lastIndex + 1]))
+                {
+                    return true;
+                }
+
+                for (int i = indices[0]; i <= indices[^1]; i++)
+                {
+                    if (IsSymbol(adjacentLine[i]))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsSymbol(char character)
+        {
+            return !char.IsNumber(character) && !char.IsLetter(character) && !character.Equals('.');
+        }
+
+        public int GetGearRatio(int charIndex, int lineIndex)
+        {
+            List<string> adjacentPartNumbers = new();
+
+            var isStartOfLine = charIndex == 0;
+            var isEndOfLine = charIndex == _data[0].Length;
+
+            List<char[]> adjacentLines = new();
+            if (lineIndex != 0) adjacentLines.Add(_data[lineIndex - 1]);
+            if (lineIndex != _data.Count - 1) adjacentLines.Add(_data[lineIndex + 1]);
+
+            if (!isStartOfLine && char.IsNumber(_data[lineIndex][charIndex - 1]))
+            {
+                var adjacentNumberIndex = charIndex - 1;
+                var adjacentNumber = GetNumberFromChars(adjacentNumberIndex, _data[lineIndex]);
+                if (IsPartNumber(new[] { adjacentNumberIndex - adjacentNumber.Length + 1, adjacentNumberIndex },
+                        _data[lineIndex], adjacentLines.ToArray()))
+                {
+                    AddToList(adjacentPartNumbers, adjacentNumber);
+                }
+            }
+
+            if (!isEndOfLine && char.IsNumber(_data[lineIndex][charIndex + 1]))
+            {
+                var adjacentNumberIndex = charIndex + 1;
+                var adjacentNumber = GetNumberFromChars(adjacentNumberIndex, _data[lineIndex]);
+
+                if (IsPartNumber(new[] { adjacentNumberIndex, adjacentNumberIndex + adjacentNumber.Length - 1 },
+                        _data[lineIndex], adjacentLines.ToArray()))
+                {
+                    AddToList(adjacentPartNumbers, adjacentNumber);
+                }
+            }
+
+            for (int adjacentLineIndex = lineIndex - 1; adjacentLineIndex <= lineIndex + 1; adjacentLineIndex += 2)
+            {
+                if (adjacentLineIndex >= 0 && adjacentLineIndex < _data.Count())
+                {
+                    var adjacentLine = _data[adjacentLineIndex];
+
+                    if (!isStartOfLine && char.IsNumber(adjacentLine[charIndex - 1]))
+                    {
+                        var adjacentNumberIndex = charIndex - 1;
+
+                        var adjacentNumber =
+                            GetNumberFromChars(adjacentNumberIndex, _data[adjacentLineIndex]);
+
+                        List<char[]> adjacentLineAdjacentLines = new();
+                        if (adjacentLineIndex != 0) adjacentLineAdjacentLines.Add(_data[adjacentLineIndex - 1]);
+                        if (adjacentLineIndex != _data.Count - 1)
+                            adjacentLineAdjacentLines.Add(_data[adjacentLineIndex + 1]);
+
+                        if (IsPartNumber(new[] { adjacentNumberIndex - adjacentNumber.Length + 1, adjacentNumberIndex },
+                                _data[adjacentLineIndex], adjacentLineAdjacentLines.ToArray()))
+                        {
+                            AddToList(adjacentPartNumbers, adjacentNumber);
+                        }
+                    }
+
+                    if (!isEndOfLine && char.IsNumber(adjacentLine[charIndex + 1]))
+                    {
+                        var adjacentNumberIndex = charIndex + 1;
+                        var adjacentNumber = GetNumberFromChars(adjacentNumberIndex, _data[adjacentLineIndex]);
+
+                        List<char[]> adjacentLineAdjacentLines = new();
+                        if (adjacentLineIndex != 0) adjacentLineAdjacentLines.Add(_data[adjacentLineIndex - 1]);
+                        if (adjacentLineIndex != _data.Count - 1)
+                            adjacentLineAdjacentLines.Add(_data[adjacentLineIndex + 1]);
+
+
+                        if (IsPartNumber(new[] { adjacentNumberIndex, adjacentNumberIndex + adjacentNumber.Length - 1 },
+                                _data[adjacentLineIndex], adjacentLineAdjacentLines.ToArray()))
+                        {
+                            AddToList(adjacentPartNumbers, adjacentNumber);
+                        }
+                    }
+
+                    if (char.IsNumber(adjacentLine[charIndex]))
+                    {
+                        var adjacentNumberIndex = charIndex;
+                        var adjacentNumber = GetNumberFromChars(adjacentNumberIndex, _data[adjacentLineIndex]);
+
+                        List<char[]> adjacentLineAdjacentLines = new();
+                        if (adjacentLineIndex != 0) adjacentLineAdjacentLines.Add(_data[adjacentLineIndex - 1]);
+                        if (adjacentLineIndex != _data.Count - 1)
+                            adjacentLineAdjacentLines.Add(_data[adjacentLineIndex + 1]);
+
+                        if (IsPartNumber(new[] { adjacentNumberIndex - adjacentNumber.Length, adjacentNumberIndex },
+                                _data[adjacentLineIndex], adjacentLineAdjacentLines.ToArray()))
+                        {
+                            AddToList(adjacentPartNumbers, adjacentNumber);
+                        }
+                    }
+                }
+            }
+
+            if (adjacentPartNumbers.Count == 2)
+            {
+                return Int32.Parse(adjacentPartNumbers[0]) * Int32.Parse(adjacentPartNumbers[1]);
+            }
+
+            return 0;
+        }
+        
+        private void AddToList(List<string> list, string itemToAdd)
+        {
+            if (!list.Contains(itemToAdd))
+            {
+                Console.WriteLine("Adding number " + itemToAdd);
+                list.Add(itemToAdd);
+            }
         }
     }
 }
